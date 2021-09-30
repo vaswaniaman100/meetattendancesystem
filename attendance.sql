24a82ffc44c18483922cbac24b1ab3279c149cc2
Aman <vaswaniaman100@gmail.com>
2021-09-30 19:10:31 +0530

    commit on 27-09-2021

--- attendance.sql
+++ attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2021 at 09:47 AM
+-- Generation Time: Sep 30, 2021 at 03:39 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -33,12 +33,22 @@ CREATE TABLE `classname` (
   `name` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `classname`
+-- Table structure for table `hibernate_sequence`
 --
 
-INSERT INTO `classname` (`id`, `name`) VALUES
-(3, 'tymca');
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(10);
 
 -- --------------------------------------------------------
 
@@ -47,9 +57,25 @@ INSERT INTO `classname` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `lectureattendance` (
+  `id` int(11) NOT NULL,
   `lectureid` int(11) NOT NULL,
   `studentname` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lectureattendance`
+--
+
+INSERT INTO `lectureattendance` (`id`, `lectureid`, `studentname`) VALUES
+(1, 31, 'Aman Vaswani'),
+(2, 39, 'Aman Vaswani'),
+(3, 39, 'Aman Vaswani'),
+(4, 40, 'Aman Vaswani'),
+(5, 40, 'Aman Vaswani'),
+(6, 41, 'Aman Vaswani'),
+(7, 41, 'Aman Vaswani'),
+(8, 42, 'Aman Vaswani'),
+(9, 42, 'Aman Vaswani');
 
 -- --------------------------------------------------------
 
@@ -61,10 +87,28 @@ CREATE TABLE `lectures` (
   `lectureid` int(11) NOT NULL,
   `subject` varchar(255) DEFAULT NULL,
   `classname` varchar(255) DEFAULT NULL,
-  `dateoflec` varchar(255) DEFAULT NULL,
+  `dateoflec` timestamp NULL DEFAULT NULL,
   `lecturecomments` varchar(255) DEFAULT NULL,
   `teachername` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lectures`
+--
+
+INSERT INTO `lectures` (`lectureid`, `subject`, `classname`, `dateoflec`, `lecturecomments`, `teachername`) VALUES
+(31, 'a', 'a', '2021-09-27 21:08:48', 'a', 'a'),
+(32, 'q', 'q', '2021-09-27 21:09:58', 'q', 'q'),
+(33, 'b', 'b', '2021-09-27 21:16:58', 'b', 'b'),
+(34, 'w', 'w', '2021-09-27 21:23:37', 'w', 'w'),
+(35, 'w', 'w', '2021-09-27 21:24:05', 'w', 'w'),
+(36, 'w', 'w', '2021-09-27 21:24:09', 'w', 'w'),
+(37, 'w', 'w', '2021-09-27 21:24:40', 'w', 'w'),
+(38, 'a', 'a', '2021-09-27 21:24:54', 'aqa', 'a'),
+(39, '', '', '2021-09-27 21:27:05', '', ''),
+(40, 'a', 'a', '2021-09-27 21:27:21', 'aman', 'a'),
+(41, 'q', 'q', '2021-09-27 21:28:44', 'pappu', 'q'),
+(42, 's', 's', '2021-09-27 21:36:06', 'aqaa', 's');
 
 -- --------------------------------------------------------
 
@@ -119,7 +163,7 @@ ALTER TABLE `classname`
 -- Indexes for table `lectureattendance`
 --
 ALTER TABLE `lectureattendance`
-  ADD PRIMARY KEY (`lectureid`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `lectures`
@@ -149,19 +193,19 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT for table `classname`
 --
 ALTER TABLE `classname`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `lectureattendance`
 --
 ALTER TABLE `lectureattendance`
-  MODIFY `lectureid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `lectures`
 --
 ALTER TABLE `lectures`
-  MODIFY `lectureid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `lectureid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `subjects`
